7d719b53fdfa1d8bcf9ee7c6ca05e489bdec6879
TikaRa <163mail@re-TikaRa.fun>
2025-11-29 03:48:52 +0800

    chore: update build scripts and ignore zip outputs

--- tools/pyinstaller.bat
+++ tools/pyinstaller.bat
@@ -6,5 +6,24 @@ rmdir /s /q dist
 echo Building...
 python -m PyInstaller DesktopTimer.spec --noconfirm
 
+echo Copying resource folders to dist...
+set "DIST_DIR=dist"
+if not exist "%DIST_DIR%" (
+    mkdir "%DIST_DIR%"
+)
+for %%D in (img lang sounds settings) do (
+    if exist "%%D" (
+        echo   - %%D
+        xcopy "%%D" "%DIST_DIR%\%%D\" /E /I /Y >nul
+    ) else (
+        echo   - Skipping %%D (folder not found)
+    )
+)
+
+echo Packaging dist into DesktopTimer.zip...
+if exist DesktopTimer.zip del /q DesktopTimer.zip
+powershell -Command "Compress-Archive -Path '%DIST_DIR%\*' -DestinationPath 'DesktopTimer.zip' -Force"
+
+:done
 echo Done.
 pause
